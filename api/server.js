f9b2a90052d33d8a3a3354a27df821ca6b50d876
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-20 13:27:40 +0100

    Cleared up future steps. Implemented and tested endpoint to retrieve list of posts for a given user

--- api/server.js
+++ api/server.js
@@ -2,14 +2,38 @@ const express = require('express');
 const morgan = require('morgan');
 
 const server = express();
+
+const Users = require('./../data/helpers/userDb');
+const Posts = require('./../data/helpers/postDb');
+
 // Middleware
 server.use(express.json());
 server.use(morgan('short'));
+// WRITE CUSTOM MIDDLEWARE TO UPPERCASE USER'S NAME BEFORE A REQUEST REACHES POST OR PUT HANDLERS
 
 // Routes
-server.get('/', (req, res) => {
-    res.send('success')
+// FIRST DO CRUD ON USERS AND POSTS!
+// USERS
+server.get('/api/user', (req, res) => {
+  
+  // .then(data => {
+  //   res.status(200).json(data);
+  // })
 })
+
+// POSTS
+
+// Create a GET to retrieve all posts from user with :id
+server.get('/api/users/:id/posts', (req, res) => {
+  const { id } = req.params;
+  Users.getUserPosts(id)
+  .then(data => {
+    res.status(200).json(data);
+  })
+}) 
+
+
+//    /api/user/1
 
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const server = require('./api/server');
 
-server.listen(4000, () => console.log(`Listening on Port 4000`))
+server.listen(4000, () => 
+    console.log(`\nListening on Port 4000\n`)
+)
